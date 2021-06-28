6391b997b1da20a30b69a5a9e427aa8a1ecd0a61
Spythere <spythere@protonmail.com>
2021-06-29 01:45:13 +0200

    Przygotowanie do migracji

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
 // module.exports = {
 //   publicPath: process.env.NODE_ENV === "production" ? "/dist" : "/",
 // };
+
+module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias.set('vue', '@vue/compat')
+
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .tap(options => {
+                return {
+                    ...options,
+                    compilerOptions: {
+                        compatConfig: {
+                            MODE: 2
+                        }
+                    }
+                }
+            })
+    }
+}
