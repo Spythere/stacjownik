e63dbb963364252081b177484fb992457e97d98d
Spythere <spythere@protonmail.com>
2022-01-10 12:53:00 +0100

    Hotfix: uwagi eksploatacyjne:

--- src/components/TrainsView/TrainSchedule.vue
+++ src/components/TrainsView/TrainSchedule.vue
@@ -16,8 +16,8 @@
 
             <span class="stop-name">
               <span v-html="stop.stopName"></span>
-              <img v-if="stop.comments" :src="icons.warning" :title="`${$t('trains.comment')}: ${stop.comments}`">
-              {{ decodeURIComponent(stop.comments) }}
+              <!-- <img v-if="stop.comments" :src="icons.warning" :title="`${$t('trains.comment')}: ${stop.comments}`"> -->
+              <!-- {{ decodeURIComponent(stop.comments) }} -->
             </span>
             <span class="stop-date">
               <span
