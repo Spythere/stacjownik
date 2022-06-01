276262f79400c6005ba0814bcd912fd1798decdb
Spythere <spythere@protonmail.com>
2022-06-02 00:32:41 +0200

    Zmiana linku API

--- src/scripts/utils/apiURLs.ts
+++ src/scripts/utils/apiURLs.ts
@@ -3,7 +3,7 @@ export const URLs = {
   sceneryDataDev: "http://127.0.0.1:8000/data",
   stations: "https://api.td2.info.pl:9640/?method=getStationsOnline",
   dispatchers: "https://api.td2.info.pl:9640/?method=readFromSWDR&value=getDispatcherStatusList%3B1",
-  stacjownikAPI: "https://stacjownik.herokuapp.com",
+  stacjownikAPI: "https://stacjownik.eu-4.evennode.com",
   stacjownikAPIDev: "http://localhost:3001"
   // trains: "https://api.td2.info.pl:9640/?method=getTrainsOnline",
   // getTimetableURL: (trainNo: string | number, region = "eu") => `https://api.td2.info.pl:9640/?method=readFromSWDR&value=getTimetable%3B${trainNo}%3B${region}`
