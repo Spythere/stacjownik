33f25e48aeffe934244130eab09550cea5c62e8b
Spythere <spythere@protonmail.com>
2022-06-29 00:24:57 +0200

    Hotfix połączenia prod.

--- src/scripts/utils/apiURLs.ts
+++ src/scripts/utils/apiURLs.ts
@@ -1,6 +1,5 @@
 export const URLs = {
-  stacjownikAPI2: 'https://stacjownik.eu-4.evennode.com',
-  stacjownikAPI: 'http://stacjownik-dev.eu-4.evennode.com',
+  stacjownikAPI: 'https://stacjownik.eu-4.evennode.com',
   stacjownikAPIDev: 'http://stacjownik-dev.eu-4.evennode.com',
   // trains: "https://api.td2.info.pl:9640/?method=getTrainsOnline",
   // getTimetableURL: (trainNo: string | number, region = "eu") => `https://api.td2.info.pl:9640/?method=readFromSWDR&value=getTimetable%3B${trainNo}%3B${region}`
