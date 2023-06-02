145dc72b6b641dde6e1f5dd7b50144b24fb16324
Spythere <spythere@protonmail.com>
2023-06-02 19:41:33 +0200

    pwa: zmiana na autoUpdate

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
 
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,svg,jpg}'],
