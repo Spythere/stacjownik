e6c064d15d8e768b8bb434900c9eb60a8494f4e8
Spythere <spythere@protonmail.com>
2025-02-13 18:38:32 +0100

    fix: reworked train stop statuses descriptions and their tooltip styles

---
+++ src/components/Tooltip/HtmlTooltip.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tooltip-content">
+    <span v-html="tooltipStore.content"></span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useTooltipStore } from '../../store/tooltipStore';
+
+export default defineComponent({
+  data() {
+    return {
+      tooltipStore: useTooltipStore()
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.tooltip-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5em;
+  white-space: pre-line;
+
+  padding: 0.25em 0.5em;
+  border-radius: 0.25em;
+
+  width: 100%;
+  background-color: #1f1f1f;
+  box-shadow: 0 0 5px 2px #aaa;
+}
+
+img {
+  height: 1em;
+}
+</style>
