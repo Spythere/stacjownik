923802e2dc633176374aad764d15158cdbdf2591
Spythere <spythere@protonmail.com>
2025-10-29 13:54:09 +0100

    chore: fetching player info data from API

--- src/views/PlayerProfileView.vue
+++ src/views/PlayerProfileView.vue
@@ -1,13 +1,37 @@
 <template>
-   <div>
-      <h2>Profil użytkownika</h2>
-   </div>
+  <div>
+    <h2>Profil użytkownika {{ route.params.id }}</h2>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { computed, onActivated, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { useApiStore } from '../store/apiStore';
 
+const apiStore = useApiStore();
+
+// Variables
+const route = useRoute();
+const playerId = computed(() => route.params.id);
+
+const playerInfo = ref({});
+
+// Lifecycle hooks
+onActivated(() => {
+  fetchPlayerInfoData();
+});
+
+// Methods
+async function fetchPlayerInfoData() {
+  if (!apiStore.client) return;
+
+  const data = (
+    await apiStore.client.get('/api/getPlayerInfo', { params: { playerId: playerId.value } })
+  ).data;
+
+  playerInfo.value = data;
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
