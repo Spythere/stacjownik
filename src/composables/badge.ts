8fd2d872b10facbe4aabbb5b7a88b12d2fb335fe
Spythere <spythere@protonmail.com>
2026-08-15 23:57:06 +0200

    chore: code cleanup; removed deprecated mixin & composable calls for calculating level styles

--- src/composables/badge.ts
+++
@@ -1,8 +0,0 @@
-export function calculateExpStyles(exp: number, isSupporter = false) {
-  const bgColor = exp >= -1 ? (exp < 2 ? '#26B0D9' : `hsl(${-exp * 3.5 + 90},  85%, 50%)`) : '#666';
-
-  const fontColor = exp == -1 || exp > 17 ? 'white' : 'black';
-  const boxShadow = isSupporter ? `box-shadow: 0 0 6px 2px ${bgColor};` : '';
-
-  return { 'background-color': bgColor, color: fontColor, 'box-shadow': boxShadow };
-}
--- src/mixins/styleMixin.ts
+++ src/mixins/styleMixin.ts
@@ -2,16 +2,6 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   methods: {
-    calculateExpStyle(exp: number, isSupporter = false): string {
-      const bgColor =
-        exp >= -1 ? (exp < 2 ? '#26B0D9' : `hsl(${-exp * 3.5 + 90},  85%, 50%)`) : '#666';
-
-      const fontColor = exp == -1 || exp > 17 ? 'white' : 'black';
-      const boxShadow = isSupporter ? `box-shadow: 0 0 6px 2px ${bgColor};` : '';
-
-      return `background-color: ${bgColor}; color: ${fontColor}; ${boxShadow};`;
-    },
-
     statusClasses(occupiedTo: string) {
       let className = '';
 
